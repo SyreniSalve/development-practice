c9ba7ea308b1b845c6fc76f5fef8fd6c0a40544e
Simona Liamo <liamosimona@yahoo.com>
2022-12-13 00:04:18 +0200

    add directory dataTypes and js files task1, task2

---
+++ src/dataTypes/task1.js
@@ -0,0 +1,13 @@
+const integerType = 1;
+const floatType = 2.2;
+const booleanType = true;
+const stringType = 'Hello';
+const charType = 'g';
+const unknownType = null;
+
+console.log(`Initial value: ${integerType}, variable type: ${typeof integerType}`);
+console.log(`Initial value: ${floatType}, variable type: ${typeof floatType}`);
+console.log(`Initial value: ${booleanType}, variable type: ${typeof booleanType}`);
+console.log(`Initial value: ${stringType}, variable type: ${typeof stringType}`);
+console.log(`Initial value: ${charType}, variable type: ${typeof charType}`);
+console.log(`Initial value: ${unknownType}, variable type: ${typeof unknownType}`);
---
+++ src/dataTypes/task2.js
@@ -0,0 +1,6 @@
+const a = '(; Hi from string line ;)';
+let b;
+const c = true;
+const d = 27;
+
+console.log(d + d + a + d);
