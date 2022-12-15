0ca6b2a6b50be0c4ab25b966047df10b32aeb14e
Simona Liamo <liamosimona@yahoo.com>
2022-12-15 14:09:02 +0200

    add directory comparisons and files task1, task2

---
+++ src/comparisons/task1.js
@@ -0,0 +1,5 @@
+const result = '5' == 5;
+console.log(result);
+
+const findBigerString = 'ABC' > 'abc';
+console.log(findBigerString);
---
+++ src/comparisons/task2.js
@@ -0,0 +1,6 @@
+let a;
+const evenOrOddNumber = a % 2 ? false : true; 
+a = 4;
+console.log(evenOrOddNumber);
+a = 5;
+console.log(evenOrOddNumber);
