3c45a660da35a56ebd85959bbe08d4969cf7b0c4
Simona Liamo <liamosimona@yahoo.com>
2022-12-16 14:45:54 +0200

    fix task2, task3

--- src/conditionalExpressions/task2.js
+++ src/conditionalExpressions/task2.js
@@ -3,5 +3,5 @@ let findValueType = typeof value;
 const result = (findValueType === 'boolean') ?  `{{Variable is boolean: ${value}}}`:
       (findValueType === 'number') ? `{{Variable is a number. It's square is: ${value ** 2}}}`:
       (findValueType === 'string') ? `{{Variable is a string: ${value}}}`:
-      `{{Variable type is: ${value}}}`
+      `{{Variable type is: ${typeof value}}}`
 console.log(result);
--- src/conditionalExpressions/task3.js
+++ src/conditionalExpressions/task3.js
@@ -5,9 +5,9 @@ let d = ' ';
 let e = '1';
 let f = 1;
 
-console.log((a > 45) ? 'true': 'false'); 
-console.log((b === null) ? 'true': 'false'); 
-console.log((c >= ' ') ? 'true': 'false'); 
-console.log((d >= ' ') ? 'true': 'false'); 
-console.log((e == 1) ? 'true': 'false'); 
-console.log((f === '1') ? 'true': 'false'); 
+console.log(a ? 'true': 'false'); 
+console.log(b ? 'true': 'false'); 
+console.log(c ? 'true': 'false'); 
+console.log(d ? 'true': 'false'); 
+console.log(e ? 'true': 'false'); 
+console.log(f ? 'true': 'false'); 
