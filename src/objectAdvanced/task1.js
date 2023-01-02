3ec05f588f03ab025378c74cb09ac3f9db470889
Simona Liamo <liamosimona@yahoo.com>
2023-01-02 17:22:10 +0200

    add in task1 bonus exercise

--- src/objectAdvanced/task1.js
+++ src/objectAdvanced/task1.js
@@ -1,20 +1,26 @@
 const obj = {
     name: 'Rutenis',
     surname: 'Turcinas',
-    age: 33
+    age: 33,
+    pet : {
+        type: 'cat',
+        name: 'Tom',
+        age: 3
+    }
 };
 
 const objInformation = (obj) => {
     for(let key in obj) {
-        console.log(`field title: ${key}, value: ${obj[key]}`);
+        if (typeof obj[key] === 'object') {
+            console.log(`field title: ${key}: `);
+            for(let newKey in obj[key]) {
+                console.log(`field title: ${newKey}, value: ${obj[key][newKey]}`); 
+            }   
+        } else {
+            console.log(`field title: ${key}, value: ${obj[key]}`);
+        }
+        
     };
-}
-
-const allfieldsKeys = (obj) => {
-    for(let o in obj) {
-        console.log(o);
-    }
-}
+};
 
 objInformation(obj);
-allfieldsKeys(obj);
