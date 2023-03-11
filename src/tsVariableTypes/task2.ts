cf2ac71c348fc06cd0726c446c7fdc6a7e9732a5
Simona Liamo <liamosimona@yahoo.com>
2023-03-11 14:09:33 +0200

    add type to function

--- src/tsVariableTypes/task2.ts
+++ src/tsVariableTypes/task2.ts
@@ -1,5 +1,5 @@
 const stringsArray: string[] = ["H", "Ha", "Hay"];
 const numArray: number[] = [];
 
-stringsArray.map((word) => numArray.push(word.length));
+stringsArray.map((word: string) => numArray.push(word.length));
 console.log(numArray);
