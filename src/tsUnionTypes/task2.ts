2ecb94e0c7f6ab3a988095e8c0811cb9f63600f1
Simona Liamo <liamosimona@yahoo.com>
2023-03-14 13:05:48 +0200

    fix functon

--- src/tsUnionTypes/task2.ts
+++ src/tsUnionTypes/task2.ts
@@ -1,6 +1,11 @@
-const fn = (argument?: string | undefined) =>
-  typeof argument === "string" ? argument.match(/[A-Z]/g)?.length : null;
+const fn = (argument?: string) =>
+  argument
+    ? argument.match(/[A-Z]/g)?.length !== undefined
+      ? argument.match(/[A-Z]/g)?.length
+      : 0
+    : null;
 
 console.log(fn("devMentor"));
 console.log(fn());
 console.log(fn("OooOoOooOoOooO"));
+console.log(fn("ooo"));
