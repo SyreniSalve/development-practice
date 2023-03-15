f782effa1fea5e12e098d918ea0e384c5070703d
Simona Liamo <liamosimona@yahoo.com>
2023-03-15 20:40:00 +0200

    add ts enums tasks

---
+++ src/tsEnums/task1.ts
@@ -0,0 +1,9 @@
+enum Color {
+  red = "#FF0000",
+  green = "#00FF00",
+  blue = "#0000FF",
+}
+
+const getColorHexCode = (color: Color) => color;
+
+console.log(getColorHexCode(Color.green));
---
+++ src/tsEnums/task2.ts
@@ -0,0 +1,24 @@
+enum Week {
+  Monday = "Monday",
+  Tuesday = "Tuesday",
+  Wednesday = "Wednesday",
+  Thursday = "Thursday",
+  Friday = "Friday",
+  Saturday = "Saturday",
+  Sunday = "Sunday",
+}
+
+enum Currencies {
+    Euros = 0.82,
+    Pounds = 0.73,
+    Yen = 108.75,
+}
+
+const whatDayToday = (day: Week) => `Today is ${day}`;
+
+console.log(whatDayToday(Week.Friday));
+
+const coverterInDollar = (amountOfMoney: number, currency: Currencies) => (amountOfMoney / currency).toFixed(2);
+
+console.log(coverterInDollar(12, Currencies. Euros));
+
